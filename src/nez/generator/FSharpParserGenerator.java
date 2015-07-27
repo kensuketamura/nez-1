0b8bee477157c4a6441efd2d28b484c4ae9922a1
kensuketamura <kensuke.tamura.183@gmail.com>
2015-07-27 15:58:48 +0900

    added FSharpParserGenerator.java

---
+++ src/nez/generator/FSharpParserGenerator.java
@@ -0,0 +1,200 @@
+package nez.generator;
+
+import nez.lang.And;
+import nez.lang.AnyChar;
+import nez.lang.Block;
+import nez.lang.ByteChar;
+import nez.lang.ByteMap;
+import nez.lang.Capture;
+import nez.lang.CharMultiByte;
+import nez.lang.Choice;
+import nez.lang.DefIndent;
+import nez.lang.DefSymbol;
+import nez.lang.ExistsSymbol;
+import nez.lang.Expression;
+import nez.lang.IsIndent;
+import nez.lang.IsSymbol;
+import nez.lang.Link;
+import nez.lang.LocalTable;
+import nez.lang.New;
+import nez.lang.NonTerminal;
+import nez.lang.Not;
+import nez.lang.Option;
+import nez.lang.Production;
+import nez.lang.Repetition;
+import nez.lang.Repetition1;
+import nez.lang.Replace;
+import nez.lang.Sequence;
+import nez.lang.Tagging;
+
+public class FSharpParserGenerator extends NezGenerator {
+
+	@Override
+	public String getDesc() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void visitEmpty(Expression p) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitFailure(Expression p) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitAnyChar(AnyChar p) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitByteChar(ByteChar p) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitByteMap(ByteMap p) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitOption(Option p) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitRepetition(Repetition p) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitRepetition1(Repetition1 p) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitAnd(And p) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitNot(Not p) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitSequence(Sequence p) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitChoice(Choice p) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitNonTerminal(NonTerminal p) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitCharMultiByte(CharMultiByte p) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitLink(Link p) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitNew(New p) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitCapture(Capture p) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitTagging(Tagging p) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitReplace(Replace p) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitBlock(Block p) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitDefSymbol(DefSymbol p) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitIsSymbol(IsSymbol p) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitDefIndent(DefIndent p) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitIsIndent(IsIndent p) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitExistsSymbol(ExistsSymbol p) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitLocalTable(LocalTable p) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitProduction(Production r) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
